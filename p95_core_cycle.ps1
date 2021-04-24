1e9ac37b66b7c2558b64fdc395b40165cbe126fc
David Thornton <dtetkt@gmail.com>
2021-04-23 22:37:43 -0500

    Update p95_core_cycle.ps1
    no need to have the license twice

--- p95_core_cycle.ps1
+++ p95_core_cycle.ps1
@@ -1,26 +1,3 @@
-# MIT License
-# 
-# Copyright (c) 2021 jasonpoly
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-
 # Windows Powershell Script
 # Will extract, configure and run a single thread on prime 95,
 # using $process.ProcessorAffinity to assign to each cpu core for the specified time
@@ -115,7 +92,7 @@ function Set-Affinity
     while ( ($runtime -lt $time_out) -and (Get-Process -Name $ProcessName -ErrorAction SilentlyContinue).Count -eq 0 )
     {        
         Start-Sleep -Milliseconds 100
-        $runtime = (NEW-TIMESPAN –Start $starttime –End (GET-DATE)).TotalSeconds
+        $runtime = (NEW-TIMESPAN â€“Start $starttime â€“End (GET-DATE)).TotalSeconds
     }
 
     if ($runtime -ge $time_out)
@@ -215,7 +192,7 @@ function Wait-prime95
     {
         Start-Sleep -Seconds $timestep
         $p95result = if (Test-Path "$work_dir\p95\results.txt") {Select-String "$work_dir\p95\results.txt" -Pattern ERROR}
-        $runtime = (NEW-TIMESPAN –Start $starttime –End (GET-DATE)).TotalSeconds
+        $runtime = (NEW-TIMESPAN â€“Start $starttime â€“End (GET-DATE)).TotalSeconds
     }
 
     if ($p95result)
